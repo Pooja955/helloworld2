8304265546ec16222473b8b47d08182dc6dff7d3
Pooja955 <pratyashasatapathy79@gmail.com>
2019-07-28 21:06:37 +0530

    firstcommit

---
+++ src/helloworld2/helloworld2.java
@@ -0,0 +1,15 @@
+//pratyasha satapathy program for print hello
+package helloworld2;
+
+public class helloworld2 {
+	/**
+	 *039 @param args
+	 */
+
+	public static void main(String[] args) {
+		// pratyasha satapathy Auto-generated method stub
+		System.out.println("Hello World");
+		//pratyasha satapathy for print
+      }
+
+}
